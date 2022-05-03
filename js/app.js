21971136ed87bb486f08c43a6fe271a0507d5038
Deniz-Navarro <ldeniz1@ucol.mx>
2022-05-03 16:31:05 -0500

    function create table

--- js/app.js
+++ js/app.js
@@ -1,16 +1,30 @@
+const main = () => {
+  let weight = prompt("Ingresa el columnas: "); //ancho
+  let height = prompt("Ingresa el filas: ");  //alto
+  let matrizTablero = crearTablero(weight, height);
+};
 
-const main = () =>{
-    let weight = prompt("Ingresa el ancho: ");
-    let height = prompt("Ingresa el alto: ");
-    let matrizTablero = crearTablero(weight,height);
-
+//Función para declarar la matriz
+function crearTablero(w,h){
+    var matriz = new Array();
+    for (let x = 0; x < w; x++) {
+        matriz[x] = new Array();
+        for (let y = 0; y < h; y++) {
+            matriz[x][y] = "-";
+        }
+        
+    }
+    console.log(matriz)
+    return matriz;
 }
-
-function crearTablero(x,y){
-  var tablero = new Array(x);
-  for(i=0;i<=x;i++){
-      tablero[i] = new Array(y);
-  }
-  return tablero;
+function reglasVida(){
+    
 }
+/*  
 
+1 toda celda viva con menos de 2 vecinos vivos muere
+2 toda celda viva con mas de 3 vecinos vivos muere
+3 toda celda viva con 2 o mas vecinos vivos permanece viva en la siguente generacion
+4 toda celda muerta con exactamente 3 vecinos muertos se convierte en una celda viva
+
+*/
